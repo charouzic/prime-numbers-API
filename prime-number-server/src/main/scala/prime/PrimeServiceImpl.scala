eaf646ccaa7eb9f93a2d64b019e3009343a93493
Viktor <viki.macek@gmail.com>
2021-01-31 10:24:44 +0100

    implementing prime service created from protocol buffers

---
+++ prime-number-server/src/main/scala/prime/PrimeServiceImpl.scala
@@ -0,0 +1,11 @@
+package prime
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
+class PrimeServiceImpl extends PrimeService {
+  override def streamSequence(in: PrimeRequest): Source[PrimeResponse, NotUsed] = {
+    lazy val primes: LazyList[Int] = 2 #:: LazyList.from(3).takeWhile(_ <= in.target).filter(i =>
+      primes.takeWhile{p => p * p <= i}.forall{ p => i % p > 0})
+    Source(primes).map(value => PrimeResponse(value))
+  }
+}
