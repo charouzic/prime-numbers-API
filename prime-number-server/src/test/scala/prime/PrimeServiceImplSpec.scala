aa59ad770ddc1295e47a3b3bd5c591b3572e84d7
Viktor <viki.macek@gmail.com>
2021-02-18 20:35:42 +0100

    fixing typo

--- prime-number-server/src/test/scala/prime/PrimeServiceImplSpec.scala
+++ prime-number-server/src/test/scala/prime/PrimeServiceImplSpec.scala
@@ -1,22 +1,20 @@
 package prime
 
-import org.scalatest._
-import org.scalatest.flatspec.AnyFlatSpec
-
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.prop.TableDrivenPropertyChecks._
 
-import scala.concurrent.duration._
 import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class PrimeServiceImplSpec extends AnyFlatSpec {
   "streamSequence" should "work as expected" in {
     implicit val sys: ActorSystem = ActorSystem("Test")
     implicit val mat: ActorMaterializer = ActorMaterializer()
-    val fibonacciServiceImpl = new PrimeServiceImpl()
+    val primeServiceImpl = new PrimeServiceImpl()
     val cases =
       Table(
         ("input", "expectedList"),
@@ -28,7 +26,7 @@ class PrimeServiceImplSpec extends AnyFlatSpec {
         (13, List(2, 3, 5, 7, 11, 13)),
         (17, List(2, 3, 5, 7, 11, 13, 17)))
     forAll(cases) { (input: Int, expected: Seq[Int]) =>
-      val primeCalc = fibonacciServiceImpl.streamSequence(new PrimeRequest(input)).map(value => value.message).runWith(Sink.seq)
+      val primeCalc = primeServiceImpl.streamSequence(new PrimeRequest(input)).map(value => value.message).runWith(Sink.seq)
       val result = Await.result(primeCalc, 3.seconds)
       result should equal(expected)
     }
